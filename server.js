7812f923e81029058cecb6cba0c6ac9339d7ec48
User Taylor <elexus0201@gmail.com>
2026-04-14 21:28:45 -0400

    Basic setup

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 //Dependencies
 const express = require('express');
-const {Pool, Client} = require("pg");
-require('dotenv').config()
+const pool = require('./db');
 
 const PORT = process.env.PORT||4000;
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
 
 const app = express();
+app.use(express.json());
 
+
+// Middleware
 app.use(express.static('public'));
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('FULL ERROR:', err);
+    res.status(500).send('Database error');
+  }
+});
 
 
 // App Listen
